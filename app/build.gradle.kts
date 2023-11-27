bbd33e69333e9acfac91c4b85b94ddfe1ce96582
Rutwikwr <rutwikwadhankar@gmail.com>
2023-11-27 17:01:27 +0530

    Displayed Home Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,19 +42,26 @@ dependencies {
 
     val nav_version = "2.7.5"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation ("com.airbnb.android:lottie:3.4.1")
 
-    // Kotlin
+    // Navigation Component
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //intuit
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
+    implementation ("com.intuit.ssp:ssp-android:1.1.0")
+
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
+
 
 }
