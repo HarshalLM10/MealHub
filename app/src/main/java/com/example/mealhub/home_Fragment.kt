d06a8a695d8c9203bac087158d71bc340d2bc314
Akash Mahato <mahatoakash897@gmail.com>
2023-12-01 22:15:13 +0530

    Added img with api from https://www.themealdb.com/api/json/v1/1/random.php in home_Fragment

--- app/src/main/java/com/example/mealhub/home_Fragment.kt
+++ app/src/main/java/com/example/mealhub/home_Fragment.kt
@@ -1,59 +1,98 @@
 package com.example.mealhub
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.mealhub.databinding.ActivityLoginBinding
+import com.example.mealhub.databinding.FragmentHomeBinding
+import com.example.mealhub.pojo.Meal
+import com.example.mealhub.pojo.MealList
+import com.example.mealhub.retrofit.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
+import kotlin.math.log
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [home_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+//// TODO: Rename parameter arguments, choose names that match
+//// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+///**
+// * A simple [Fragment] subclass.
+// * Use the [home_Fragment.newInstance] factory method to
+// * create an instance of this fragment.
+// */
 class home_Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentHomeBinding
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
         }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment home_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            home_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        RetrofitInstance.api.getRandomMeal().enqueue(object : retrofit2.Callback<MealList>{
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                if(response.body()!=null){
+                    val randomMeal: Meal = response.body()!!.meals[0]
+                    Glide.with(this@home_Fragment)
+                        .load(randomMeal.strMealThumb)
+                        .into(binding.randomMeal)
+
+                }else{
+                    return
                 }
             }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.d("home_Fragment",t.message.toString())
+            }
+        } )
+
+
+
     }
 }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment home_Fragment.
+//         */
+        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            home_Fragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+//}
---
+++ app/src/main/java/com/example/mealhub/retrofit/MealApi.kt
@@ -0,0 +1,10 @@
+package com.example.mealhub.retrofit
+
+import com.example.mealhub.pojo.MealList
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MealApi {
+    @GET("random.php")
+    fun getRandomMeal():Call<MealList>
+}
